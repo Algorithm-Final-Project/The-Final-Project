00e49f83604417049392597663b717bda5436a3a
bsirikam <buntakan0703@gmail.com>
2023-04-02 02:37:26 +0700

    BNS Complete

--- bs.py
+++ bs.py
@@ -4,3 +4,24 @@ class	BinarySeach:
 	def	__init__(self, target, arr):
 		self.target = target
 		self.arr = arr
+	def	bns(self, left, mid, right, arr, target):
+		if left > right:
+			return -1
+		mid = (left + right) // 2
+		if arr[mid] == target:
+			return mid
+		elif arr[mid] < target:
+			return self.bns(mid + 1, mid, right, arr, target)
+		else:
+			return self.bns(left, mid, mid - 1, arr, target)
+
+if __name__ == "__main__":
+	data = [1, 2, 3, 4, 30]
+	utl = Util()
+	node = None
+	new = None
+	for i in range(len(data)):
+		node = utl.add_back(node, data[i])
+	# utl.print_lst(node)
+	bns = BinarySeach(30, data)
+	print(bns.bns(0, 0, len(data) - 1, data, 30))
--- linked_list.py
+++ linked_list.py
@@ -1,7 +1,10 @@
 class	Node:
+	i = 0
 	def	__init__(self, data):
+		Node.i += 1
 		self.data = data
 		self.next = None
+		self.id = Node.i
 
 class	Util:
 	def	add_back(self, lst : Node, data):
@@ -15,14 +18,15 @@ class	Util:
 		if lst == None:
 			return
 		else:
-			print(lst.data)
+			print("Data =", lst.data)
+			print("ID =", lst.id)
 			self.print_lst(lst.next)
 
-if __name__ == "__main__":
-	data = [1, 2, 3]
-	i = 0
-	utl = Util()
-	lst = None
-	for i in range(len(data)):
-		new = Node(data[i])
-		lst = utl.add_back(lst, new.data)
+# if __name__ == "__main__":
+# 	data = [1, 2, 3]
+# 	i = 0
+# 	utl = Util()
+# 	lst = None
+# 	for i in range(len(data)):
+# 		new = Node(data[i])
+# 		lst = utl.add_back(lst, new.data)
