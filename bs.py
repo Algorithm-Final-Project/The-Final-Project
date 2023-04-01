686b41fc9abbfa037db967443ff7c16b6a575c7f
bsirikam <buntakan0703@gmail.com>
2023-04-01 17:58:51 +0700

    Add

--- bs.py
+++ bs.py
@@ -1,30 +1,6 @@
-class	Node:
-	def	__init__(self, data):
-		self.data = data
-		self.next = None
+from linked_list import Node, Util
 
-class	Util:
-	def	add_back(self, lst : Node, data):
-		if lst == None:
-			return Node(data)
-		else:
-			lst.next = self.add_back(lst.next, data)
-			return lst
-
-	def	print_lst(self, lst : Node):
-		if lst == None:
-			return
-		else:
-			print(lst.data)
-			self.print_lst(lst.next)
-
-if __name__ == "__main__":
-	data = [1, 2, 3]
-	i = 0
-	utl = Util()
-	lst = None
-	for i in range(len(data)):
-		new = Node(data[i])
-		lst = utl.add_back(lst, new.data)
-
-	utl.print_lst(lst)
+class	BinarySeach:
+	def	__init__(self, target, arr):
+		self.target = target
+		self.arr = arr
