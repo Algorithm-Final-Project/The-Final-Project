027823a743fcbc16fef727a03322f1615b8e6f25
bsirikam <buntakan0703@gmail.com>
2023-04-04 00:37:50 +0700

    Final

--- bs.py
+++ bs.py
@@ -1,4 +1,5 @@
-from linked_list import Node, Util
+import os
+from art import *
 
 class	BinarySeach:
 	total = 0
@@ -8,40 +9,68 @@ class	BinarySeach:
 		'Mid' : [],
 		'Right' : []
 	}
-	def	__init__(self, target, arr):
-		self.target = target
-		self.arr = arr
+	def	__init__(self):
+		pass
 	def	bns(self, left, mid, right, arr, target):
 		self.total += 1
-		self.data['Total'].append(self.total)
 		self.data['Left'].append(left)
 		self.data['Mid'].append(mid)
 		self.data['Right'].append(right)
 		if left > right:
-			return -1, data
+			self.data['Total'] = self.total
+			return -1, self.data
 		mid = (left + right) // 2
 		if arr[mid] == target:
-			return mid, data
+			self.data['Total'] = self.total
+			return mid, self.data
 		elif arr[mid] < target:
 			return self.bns(mid + 1, mid, right, arr, target)
 		else:
 			return self.bns(left, mid, mid - 1, arr, target)
+	def	check_sort(self, arr):
+		for i in range(len(arr) - 1):
+			if arr[i] > arr[i + 1]:
+				return False
+		return True
+	def	check_num(self, arr):
+		for i in arr:
+			if i.isnumeric() == False:
+				return False
+		return True
+	def	bye(self):
+		tprint("Thank you")
+		exit()
 
 if __name__ == "__main__":
-	data = [1, 2, 3, 4, 3]
-	utl = Util()
-	node = None
-	new = None
-	for i in range(len(data)):
-		node = utl.add_back(node, data[i])
-	# utl.print_lst(node)
-	bns = BinarySeach(30, data)
-	index, total = bns.bns(0, 0, len(data) - 1, data, 30)
-	if (index != -1):
-		print("Found at index", index, "with total", total)
+	tprint("Binary", font = "block")
+	tprint("Search", font = "block")
+	os.system("sleep 5")
+	os.system("clear")
+	bn = BinarySeach()
+	a = input("Enter Input : ").split(",")
+	if (bn.check_num(a) == False):
+		print("Not Number")
+		os.system("sleep 3")
+		bn.bye()
+		exit()
+	if (bn.check_sort(a) == False):
+		print("Not Sorted")
+		os.system("sleep 3")
+		bn.bye()
+		exit()
+	b = input("Enter Target : ")
+	os.system("clear")
+	index, data = bn.bns(0, 0, len(a) - 1, a, b)
+	if index == -1:
+		tprint("Not Found")
+		os.system("sleep 3")
+		bn.bye()
+		exit()
 	else:
-		print("Not found with total", total)
-	# print(bns.bns(0, 0, len(data) - 1, data, 30))
-	# print("Index =", index)
-	# print("Total =", total)
-	# print(bns.data)
+		print("Found at index", index)
+		print("Total", data['Total'], "times")
+		count = 0
+		for left, mid, right in zip(data['Left'], data['Mid'], data['Right']):
+			count += 1
+			print("Round", count, ":", end = " ")
+			print("Left =", left, "Mid =", mid, "Right =", right)
