a2a76d2f0e7ec9b4c91a10498718347b5bc0ce18
bsirikam <buntakan0703@gmail.com>
2023-04-03 21:27:22 +0700

    BNS done

--- bs.py
+++ bs.py
@@ -1,22 +1,34 @@
 from linked_list import Node, Util
 
 class	BinarySeach:
+	total = 0
+	data = {
+		'Total' : [],
+		'Left' : [],
+		'Mid' : [],
+		'Right' : []
+	}
 	def	__init__(self, target, arr):
 		self.target = target
 		self.arr = arr
 	def	bns(self, left, mid, right, arr, target):
+		self.total += 1
+		self.data['Total'].append(self.total)
+		self.data['Left'].append(left)
+		self.data['Mid'].append(mid)
+		self.data['Right'].append(right)
 		if left > right:
-			return -1
+			return -1, data
 		mid = (left + right) // 2
 		if arr[mid] == target:
-			return mid
+			return mid, data
 		elif arr[mid] < target:
 			return self.bns(mid + 1, mid, right, arr, target)
 		else:
 			return self.bns(left, mid, mid - 1, arr, target)
 
 if __name__ == "__main__":
-	data = [1, 2, 3, 4, 30]
+	data = [1, 2, 3, 4, 3]
 	utl = Util()
 	node = None
 	new = None
@@ -24,4 +36,12 @@ if __name__ == "__main__":
 		node = utl.add_back(node, data[i])
 	# utl.print_lst(node)
 	bns = BinarySeach(30, data)
-	print(bns.bns(0, 0, len(data) - 1, data, 30))
+	index, total = bns.bns(0, 0, len(data) - 1, data, 30)
+	if (index != -1):
+		print("Found at index", index, "with total", total)
+	else:
+		print("Not found with total", total)
+	# print(bns.bns(0, 0, len(data) - 1, data, 30))
+	# print("Index =", index)
+	# print("Total =", total)
+	# print(bns.data)
